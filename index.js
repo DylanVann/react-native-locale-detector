e4e0c5a59ccbb2fa9af9edfe496df18fa390378e
Dylan Vann <dylan.vann@gmail.com>
2016-09-12 03:36:13 -0400

    Use ASI

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const { NativeModules } = require('react-native');
-const { RNI18n } = NativeModules;
+const { NativeModules } = require('react-native')
+const { RNI18n } = NativeModules
 
-const locale = (RNI18n) ? RNI18n.locale.replace(/_/, '-') : '';
-
-module.exports = locale;
+export default (RNI18n) ? RNI18n.locale.replace(/_/, '-') : ''
